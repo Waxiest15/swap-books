34a5eb16590fc860ac28dd73caa549bb8c5b3db8
fernando <fernando.robles@harley-davidson.com>
2026-02-20 21:46:41 -0600

    [add]: DRM for "Books" entity

---
+++ swap-books/src/main/java/com/fernando/robles/swap_books/model/Books.java
@@ -0,0 +1,28 @@
+package com.fernando.robles.swap_books.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document("books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Books {
+    @Id
+    private Long ISBN;
+    private String title;
+    private String author;
+    private List<String> genres;
+    private List<String> tags;
+    private int pages;
+    private String publisher;
+    private String publicationDate;
+    private String condition;
+    private String conditionDescription;
+    private String description;
+}
