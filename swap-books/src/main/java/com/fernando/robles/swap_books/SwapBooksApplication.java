f9c1dc66c45e2b4c529a53f3093a4f97bc4b29fb
waxiest15 <waxiest15@gmail.com>
2026-02-24 20:00:10 -0600

    [add]: enable Mongo repositories to work in Spring

--- swap-books/src/main/java/com/fernando/robles/swap_books/SwapBooksApplication.java
+++ swap-books/src/main/java/com/fernando/robles/swap_books/SwapBooksApplication.java
@@ -2,10 +2,11 @@ package com.fernando.robles.swap_books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SwapBooksApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SwapBooksApplication.class, args);
 	}
