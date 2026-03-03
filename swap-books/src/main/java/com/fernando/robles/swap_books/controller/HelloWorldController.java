8e1b14295b35ea473204517b40b3dd9f9b21c07f
waxiest15 <waxiest15@gmail.com>
2026-03-03 17:32:37 -0600

    [add]: endpoint to test API calls

---
+++ swap-books/src/main/java/com/fernando/robles/swap_books/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.fernando.robles.swap_books.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello, World!";
+    }
+}
