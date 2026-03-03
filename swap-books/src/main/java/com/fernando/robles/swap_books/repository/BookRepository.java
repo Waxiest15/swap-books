ae46a06869131ff145a89ae3a451c97e0e67a7ef
waxiest15 <waxiest15@gmail.com>
2026-03-03 14:16:56 -0600

    [update]: add @Repository so it gets scanned by the IoC

--- swap-books/src/main/java/com/fernando/robles/swap_books/repository/BookRepository.java
+++ swap-books/src/main/java/com/fernando/robles/swap_books/repository/BookRepository.java
@@ -1,11 +1,13 @@
-package com.fernando.robles.swap_books;
+package com.fernando.robles.swap_books.repository;
 
 import com.fernando.robles.swap_books.model.Books;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends MongoRepository<Books, Long> {
 
     @Query("{ '_id': ?0 }")
